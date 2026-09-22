14f154df308a957900ae4c7c2233ed113b609faf
NANA-QUAMI1142 <arkohm662@gmai.com>
2026-09-22 11:59:36 +0000

    fix dotenv clash

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
-require('dotenv').config();
+try { require('dotenv').config(); } catch(e) {}
 
 const app = express();
 app.use(express.json());
